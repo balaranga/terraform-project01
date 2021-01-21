1db0af59cde914ca4a7f38c275e0873cbf957502
bala <balajangala@gmail.com>
2021-01-21 08:50:09 +0000

    adding vpc module

--- main.tf
+++ main.tf
@@ -1,27 +1,9 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_key_pair" "example" {
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEoHE1svLswPWIFuMB49kbm173nRgof1ILv9vQ002aXyUX5j/VER4vFzyjCEKCqA6f2wK+v0KcpgJ1YY6UK2TcYO2goJzxHzC/50SJ5MujK//QhkWwVdgrhmUQxdo/F2t0J7K9DASby6xp0QPm9DaxIiWFwIIciHjWItJT86lBtGZcYt4vp6lb2HjK1D5FYwqghBW1eJWLLVtBnOJ5l0bf56FpFQ9XYNAqnUsKGNSnCO2ARkwnnriXeGldNiU9xdxLtpXrDlRFsX5+hM7fYOFawxs1llmPie65CAD6sfj5CqRnhrmD5TbJakJuUzu9f8FzBTfoIvsUcLQXh2WXi0d7 vagrant@terraforminstance"
+module "vpc" {
+  source        = "./vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  public_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
 }
-
-resource "aws_security_group" "examplesg" {
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_instance" "ec2instance" {
-  ami                    = "ami-0a4a70bd98c6d6441"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.examplesg.id}"]
-  key_name               = aws_key_pair.example.id
-  tags = {
-    "Name" = "my-webserver-instance"
-  }
-}
-
